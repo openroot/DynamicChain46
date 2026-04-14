7ba45ec09b8d3b3fa2e565ebf9fd690b1da4be93
Debaprasad Tapader <dev.openroot@gmail.com>
2026-04-15 00:04:23 +0530

    update

--- DynamicChain46/MainWindow.xaml.cs
+++ DynamicChain46/MainWindow.xaml.cs
@@ -2,19 +2,22 @@
 
 namespace DynamicChain46
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			Learn.Study myStudy = new Learn.Study();
-			Trade.Share aShare = new Trade.Share();
-			Network.Channel newChannel = new Network.Channel();
-			Information.Subject firstSubject = new Information.Subject();
+			Learn.Study lifeScience = new Learn.Study();
+			//Software.Compile drawAlbum = new Software.Compile();
+			Information.Subject firstIdea = new Information.Subject();
+			Network.Channel liveAction = new Network.Channel();
+			Trade.Share customMenu = new Trade.Share();
+			//Structure.Design projectRelay = new Structure.Design();
+			//Business.Process autoStart = new Business.Process();
+			//Profession.Statement netSummary = new Profession.Statement();
+			//Database.Data logicGate = new Database.Data();
+			//Hardware.Execute pointCart = new Hardware.Execute();
 		}
 	}
 }
